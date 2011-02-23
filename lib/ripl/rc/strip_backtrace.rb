ecbf2478b671c80ea6bcbe61841e8bb5b70f6f0d
Lin Jen-Shin <godfat@godfat.org>
2011-02-24 03:36:59 +0800

    strip_backtrace.rb: try to short the path with cwd or home

--- lib/ripl/rc/strip_backtrace.rb
+++ lib/ripl/rc/strip_backtrace.rb
@@ -2,12 +2,27 @@
 module Ripl::Rc::StripBacktrace
   # strip backtrace until ripl
   def format_error e
-    "#{e.class}: #{e.message}\n  #{strip_backtrace(e).join("\n  ")}"
+    "#{e.class}: #{e.message}\n  #{U.strip_backtrace(e).join("\n  ")}"
   end
 
-  def strip_backtrace e
-    e.backtrace[0..e.backtrace.rindex{ |l| l =~ /\(ripl\):\d+:in `.+?'/ } ||
-                   -1]
+  module U
+    module_function
+    def strip_backtrace e
+      home(cwd(snip(e)))
+    end
+
+    def home b
+      b.map{ |p| p.sub(ENV['HOME'], '~') }
+    end
+
+    def cwd b
+      b.map{ |p| p.sub(Dir.pwd, './') }
+    end
+
+    def snip e
+      e.backtrace[0..e.backtrace.rindex{ |l| l =~ /\(ripl\):\d+:in `.+?'/ } ||
+                     -1]
+    end
   end
 end
 
