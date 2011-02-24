beab8d15aaa4e11a6a184e92db3a90a36bb32588
Lin Jen-Shin <godfat@godfat.org>
2011-02-25 04:40:32 +0800

    squeeze_history.rb: always write history, but only load if empty?

--- lib/ripl/rc/squeeze_history.rb
+++ lib/ripl/rc/squeeze_history.rb
@@ -9,7 +9,7 @@ module Ripl::Rc::SqueezeHistory
   def write_history
     File.open(history_file, 'w'){ |f|
       f.puts U.squeeze_history(history).join("\n")
-    } if (Ripl.config[:rc_anchor] || []).empty?
+    }
   end
 
   # squeeze history on memory too
@@ -20,7 +20,7 @@ module Ripl::Rc::SqueezeHistory
   end
 
   def before_loop
-    super if (Ripl.config[:rc_anchor] || []).empty?
+    super if history.empty?
   end
 
   module Imp
