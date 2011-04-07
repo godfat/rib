26d8ce7515b8fd6004b89b0c24e99303c9fd6f27
Lin Jen-Shin <godfat@godfat.org>
2011-04-07 21:52:24 +0800

    test_disable_shortcut.rb: make sure they are all the same

--- test/test_disable_shortcut.rb
+++ test/test_disable_shortcut.rb
@@ -6,18 +6,38 @@ Bacon.summary_on_exit
 include RR::Adapters::RRMethods
 
 describe Ripl::Rc::U do
-  should 'have shortcut methods' do
-    mods = Dir[File.expand_path(
-                 "#{File.dirname(__FILE__)}/../lib/ripl/rc/*.rb")].
-                 map   { |path| File.basename(path)[0..-4] }.
-                 reject{ |name| %w[version u noirbrc].include?(name) }
+  before do
+    @names = Dir[File.expand_path(
+               "#{File.dirname(__FILE__)}/../lib/ripl/rc/*.rb")].
+               map   { |path| File.basename(path)[0..-4] }.
+               reject{ |name| %w[version u noirbrc].include?(name) }
+    @mods  = Ripl::Shell.ancestors[1..-1].select{ |mod| mod < Ripl::Rc }
+  end
 
-    mods.each{ |mod|
+  after do
+    @mods.each(&:enable)
+  end
+
+  should 'have shortcut methods' do
+    @names.each{ |name|
       %w[enable disable].each{ |meth|
-        Ripl.should.respond_to?("#{meth}_#{mod}") == true
+        Ripl.should.respond_to?("#{meth}_#{name}") == true
       }
       %w[enabled? disabled?].each{ |meth|
-        Ripl.should.respond_to?("#{mod}_#{meth}") == true
+        Ripl.should.respond_to?("#{name}_#{meth}") == true
+      }
+    }
+  end
+
+  should 'be the same as mod methods' do
+    @mods.shuffle.take(@mods.size/2).each(&:disable)
+    @names.each{ |name|
+      %w[enabled? disabled?].each{ |meth|
+        Ripl.send("#{name}_#{meth}").should ==
+          @mods.find{ |mod|
+            mod.name[/::\w+$/].tr(':', '') ==
+            name.gsub(/([^_]+)/){$1.capitalize}.tr('_', '') }.
+          send(meth)
       }
     }
   end
