558cf6c9ae1691c64774b67ec3094a9ad8e87545
Lin Jen-Shin <godfat@godfat.org>
2011-08-05 06:15:51 +0800

    restore color test

---
+++ lib/rib/test.rb
@@ -0,0 +1,8 @@
+
+require 'bacon'
+require 'rr'
+require 'fileutils'
+Bacon.summary_on_exit
+include RR::Adapters::RRMethods
+
+require 'rib'
--- test/test_color.rb
+++ test/test_color.rb
@@ -1,10 +1,19 @@
 
-require 'ripl/rc/test'
-require 'ripl/rc/color'
+require 'rib/test'
+require 'rib/more/color'
 
-describe Ripl::Rc::Color do
+describe Rib::Color do
+  before do
+    @color = Class.new do
+      include Rib::Color
+      def colors
+        @colors ||= Rib::Shell.new.before_loop.config[:color]
+      end
+    end.new
+  end
+
   should 'give correct color' do
-    Ripl::Rc::Color.format_result(
+    @color.send(:format_color,
       [{0 => :a}, 'b', [nil, {false => Object}], {true => Exception.new}]).
         should ==
           "\e[34m[\e[m\e[34m{\e[m\e[31m0\e[m\e[34m=>\e[m\e[36m:a\e[m\e[" \
