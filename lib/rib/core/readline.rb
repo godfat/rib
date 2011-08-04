027dc7f4272a465e04d6bdb8328c184c36b91c62
Lin Jen-Shin <godfat@godfat.org>
2011-08-05 05:04:26 +0800

    readline could be skipped too

--- lib/rib/core/readline.rb
+++ lib/rib/core/readline.rb
@@ -7,11 +7,13 @@ module Rib::Readline
   Shell.use(self)
 
   def before_loop
+    return super if Readline.disabled?
     @history = ::Readline::HISTORY
     super
   end
 
   def get_input
+    return super if Readline.disabled?
     ::Readline.readline(prompt, true)
   end
 end
