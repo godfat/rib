602d87fc2ba100a17849a723c02cf73af28ea4b0
Lin Jen-Shin <godfat@godfat.org>
2011-08-06 00:31:05 +0800

    don't pollute namespace

--- test/test_api.rb
+++ test/test_api.rb
@@ -7,8 +7,11 @@ describe Rib::API do
 
   Rib::API.instance_methods.delete_if{ |e| e[/=$/] }.each do |meth|
     should "##{meth} be accessible to plugins" do
-      mod = Object.const_set "Ping_#{meth}", Module.new
-      mod.send(:define_method, meth) { "pong_#{meth}" }
+      mod = Module.new do
+        define_method meth do
+          "pong_#{meth}"
+        end
+      end
       shell = Rib::Shell.dup
       shell.use(mod)
       shell.new.send(meth).should == "pong_#{meth}"
