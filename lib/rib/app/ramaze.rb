88e7aad97bb98fb53d06da89f77e699af2a792ad
Lin Jen-Shin <godfat@godfat.org>
2011-08-06 07:33:54 +0800

    ramaze.rb: separate loading and rescuing

--- lib/rib/app/ramaze.rb
+++ lib/rib/app/ramaze.rb
@@ -3,6 +3,12 @@ module Rib; end
 module Rib::Ramaze
   module_function
   def load
+    load_ramaze
+  rescue LoadError => e
+    Rib.abort("Is this a Ramaze app?\n  #{e}")
+  end
+
+  def load_ramaze
     # try to produce consistent error message, and yet lazy loading ramaze
     require './start' unless File.exist?('./start.rb')
 
@@ -11,8 +17,5 @@ module Rib::Ramaze
 
     require './start'
     at_exit{ puts('Ramazement has ended, go in peace.') }
-
-  rescue LoadError => e
-    Rib.abort("Is this a Ramaze app?\n  #{e}")
   end
 end
