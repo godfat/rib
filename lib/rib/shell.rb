fcedcf306ca36ea6a1a4c30674d79d7c6641889f
Lin Jen-Shin <godfat@godfat.org>
2011-08-05 23:24:16 +0800

    move Rip::Shell::API to Rip::API and a new file

--- lib/rib/shell.rb
+++ lib/rib/shell.rb
@@ -1,7 +1,9 @@
 
 require 'rib/plugin'
+require 'rib/api'
 
 class Rib::Shell
+  include Rib::API
   trap('INT'){ raise Interrupt }
 
   def self.use mod
@@ -38,107 +40,4 @@ class Rib::Shell
   protected
   attr_writer :config
   attr_accessor :error_raised
-
-  module API
-    def before_loop
-      self
-    end
-
-    def in_loop
-      input = catch(:rib_exit){ loop_once while true }
-      puts if input == nil
-    end
-
-    # Runs through one loop iteration: gets input, evals and prints result
-    def loop_once
-      self.error_raised = nil
-      input = get_input
-      throw(:rib_exit, input) if config[:exit].include?(input)
-      if input.strip == ''
-        eval_input(input)
-      else
-        print_result(eval_input(input))
-      end
-    rescue Interrupt
-      handle_interrupt
-    end
-
-    # Handles interrupt (Control-C) by printing a newline
-    def handle_interrupt() puts end
-
-    # Sets @result to result of evaling input and print unexpected errors
-    def eval_input(input)
-      loop_eval(input)
-    rescue Exception => e
-      self.error_raised = true
-      print_eval_error(e)
-    ensure
-      config[:line] += 1
-    end
-
-    # When extending this method, ensure your plugin disables readline:
-    # Readline.config[:readline] = false.
-    # @return [String, nil] Prints #prompt and returns input given by user
-    def get_input
-      print(prompt)
-      if input = $stdin.gets
-        input.chomp
-      else
-        nil
-      end
-    end
-
-    # @return [String]
-    def prompt
-      config[:prompt]
-    end
-
-    def read_history
-    end
-
-    def write_history
-    end
-
-    def history
-    end
-
-    # Evals user input using @binding, @name and @line
-    def loop_eval(input)
-      config[:binding].eval(input, "(#{config[:name]})", config[:line])
-    end
-
-    # Prints error formatted by #format_error to STDERR. Could be extended to
-    # handle certain exceptions.
-    # @param [Exception]
-    def print_eval_error(err)
-      warn format_error(err)
-    end
-
-    # Prints result using #format_result
-    def print_result(result)
-      puts(format_result(result)) unless error_raised
-    rescue StandardError, SyntaxError => e
-      warn "#{config[:name]}: Error while printing result:\n" \
-           "#{format_error(e)}"
-    end
-
-    # Formats errors raised by eval of user input
-    # @param [Exception]
-    # @return [String]
-    def format_error(e)
-      "#{e.class}: #{e.message}\n    #{e.backtrace.join("\n    ")}"
-    end
-
-    # @return [String] Formats result using result_prompt
-    def format_result(result)
-      config[:result_prompt] + result.inspect
-    end
-
-    # Called after shell finishes looping.
-    def after_loop
-      self
-    end
-  end
-
-  include API
 end
--- test/test_api.rb
+++ test/test_api.rb
@@ -2,10 +2,10 @@
 require 'rib/test'
 require 'rib/shell'
 
-describe Rib::Shell::API do
+describe Rib::API do
   behaves_like :rib
 
-  Rib::Shell::API.instance_methods.delete_if{ |e| e[/=$/] }.each do |meth|
+  Rib::API.instance_methods.delete_if{ |e| e[/=$/] }.each do |meth|
     should "##{meth} be accessible to plugins" do
       mod = Object.const_set "Ping_#{meth}", Module.new
       mod.send(:define_method, meth) { "pong_#{meth}" }
