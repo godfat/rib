51073c9076f532a1d1483508f3dd7b91d6c3eeb1
Lin Jen-Shin <godfat@godfat.org>
2011-08-05 22:56:40 +0800

    underscore.rb: greatly enhanced, eliminate various warnings, avoid:
    shadowing original underscore methods

--- lib/rib/core/underscore.rb
+++ lib/rib/core/underscore.rb
@@ -5,29 +5,49 @@ module Rib::Underscore
   include Rib::Plugin
   Shell.use(self)
 
+  IVAR = {:_  => :@__rib_result__,
+          :__ => :@__rib_exception__}
+
+  def before_loop
+    return super if Underscore.disabled?
+    eliminate_warnings
+    inject_inspecter
+    super
+  end
+
   def loop_eval input
     return super if Underscore.disabled?
-    value = super
-    bound_object.send(:define_method, :_) do
-      value
-    end
-    value
+    bound_object.instance_variable_set(:@__rib_result__, super)
   end
 
   def print_eval_error e
     return super if Underscore.disabled?
-    bound_object.send(:define_method, :__) do
-      e
-    end
+    bound_object.instance_variable_set(:@__rib_exception__, e)
     super
   end
 
   private
-  def bound_object
+  def eliminate_warnings
+    IVAR.values.each{ |ivar| bound_object.instance_variable_set(ivar, nil) }
+  end
+
+  def inject_inspecter
+    IVAR.each{ |k, v|
+      bound_singleton.send(:define_method, k){
+        instance_variable_get(v)
+      } unless bound_object.respond_to?(k) # only inject for innocences
+    }
+  end
+
+  def bound_singleton
     if respond_to?(:singleton_class)
-      config[:binding].eval('self').singleton_class
+      bound_object.singleton_class
     else
-      class << config[:binding].eval('self'); self; end
+      class << bound_object; self; end
     end
+  end
+
+  def bound_object
+    config[:binding].eval('self')
   end
 end
