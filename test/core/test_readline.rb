47d7cc19078a51086e2153a413d9970ad48db0da
Lin Jen-Shin <godfat@godfat.org>
2011-08-13 02:33:20 +0800

    test_readline.rb: join the test burst party

--- test/core/test_readline.rb
+++ test/core/test_readline.rb
@@ -2,15 +2,7 @@
 require 'rib/test'
 require 'rib/core/readline'
 
-describe Rib::Readline do
-  behaves_like :rib
-
-  before do
-    Rib.disable_plugins
-    Rib::Readline.enable
-    @shell = Rib::Shell.new.before_loop
-  end
-
+shared :readline do
   should '#before_loop set @history' do
     @shell.history.should.eq Readline::HISTORY
   end
@@ -18,5 +10,17 @@ describe Rib::Readline do
   should '#get_input calling Readline.readline' do
     mock(Readline).readline(@shell.prompt, true){'ok'}
     @shell.get_input.should.eq 'ok'
+  end
+end
+
+describe Rib::Readline do
+  behaves_like :rib
+
+  before do
+    @shell = Rib::Shell.new.before_loop
+  end
+
+  test_for Rib::Readline do
+    behaves_like :readline
   end
 end
