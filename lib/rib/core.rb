8f7d64a54366a5046fcc8423335d7e0d75aa28e5
Lin Jen-Shin <godfat@godfat.org>
2011-08-13 21:50:58 +0800

    move strip_backtrace plugin into core

--- lib/rib/core.rb
+++ lib/rib/core.rb
@@ -1,6 +1,16 @@
 
+# before session starts
 require 'rib/core/completion'
+
+# upon session ends
 require 'rib/core/history'
-require 'rib/core/multiline'
+
+# upon formatting output
+require 'rib/core/strip_backtrace'
+
+# upon input
 require 'rib/core/readline'
+require 'rib/core/multiline'
+
+# special tools
 require 'rib/core/underscore'
--- lib/rib/more.rb
+++ lib/rib/more.rb
@@ -4,7 +4,6 @@ require 'rib/more/squeeze_history'
 require 'rib/more/multiline_history_file'
 
 # upon formatting output
-require 'rib/more/strip_backtrace'
 require 'rib/more/color'
 
 # upon input
