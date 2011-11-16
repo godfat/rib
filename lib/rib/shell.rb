f4248a66cedc5272c389efe7fd3cdf4db57923d6
Lin Jen-Shin <godfat@godfat.org>
2011-11-17 05:33:56 +0800

    shell.rb: get rid of `quit' command

--- lib/rib/shell.rb
+++ lib/rib/shell.rb
@@ -13,12 +13,11 @@ class Rib::Shell
   attr_reader :config
   def initialize(config={})
     self.config = {
-      :result_prompt => '=> '                ,
-      :prompt        => '>> '                ,
-      :binding       => TOPLEVEL_BINDING     ,
-      :exit          => [nil, 'exit', 'quit'],
-      :line          => 1
-    }.merge(config)
+      :result_prompt => '=> '           ,
+      :prompt        => '>> '           ,
+      :binding       => TOPLEVEL_BINDING,
+      :exit          => [nil, 'exit']   ,
+      :line          => 1               }.merge(config)
     @running = false
   end
 
