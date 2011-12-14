5f901ed44d6ed6564578a6c44b067844a6b5e4a0
Lin Jen-Shin <godfat@godfat.org>
2011-12-15 02:06:17 +0800

    Revert "rib/runner.rb: introduce -s, --stdin option which is like -e"
    This reverts commit 05bd190a2144c538fbda45052df318422d0881c8.
    
    This does not really make sense... :s

--- lib/rib/runner.rb
+++ lib/rib/runner.rb
@@ -22,7 +22,6 @@ module Rib::Runner
        'Require the library, before executing your script'           ],
 
      ['rib options:'     , ''                                        ],
-     ['-s, --stdin'      , 'Evaluate codes from $stdin'              ],
      ['-c, --config FILE', 'Load config from FILE'                   ],
      ['-n, --no-config'  , 'Suppress loading ~/.config/rib/config.rb'],
      ['-h, --help'       , 'Print this message'                      ],
@@ -98,9 +97,6 @@ module Rib::Runner
 
       when /^-r=?(.+)?/, /^--require=?(.+)?/
         require($1 || argv.shift)
-
-      when /^-s/, '--stdin'
-        eval($stdin.read, TOPLEVEL_BINDING, __FILE__, __LINE__)
 
       when /^-c=?(.+)?/, /^--config=?(.+)?/
         Rib.config[:config] = $1 || argv.shift
