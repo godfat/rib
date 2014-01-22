cb34fe819b51895eddee296847d55065414bd515
Lin Jen-Shin <godfat@godfat.org>
2014-01-23 01:31:45 +0800

    more api documents

--- lib/rib.rb
+++ lib/rib.rb
@@ -8,6 +8,8 @@ module Rib
   module_function
   # All default Rib configs, would be passed to Shell.new in Rib.shell,
   # but calling Shell.new directly won't bring this in.
+  #
+  # @api public
   def config
     @config ||= {:config => File.join(home, 'config.rb'), :name => 'rib'}
   end
@@ -34,8 +36,10 @@ module Rib
   end
 
   # Convenient shell accessor, which would just give you current last shell
-  # or create one and load ~/.config/rib/config.rb if non has existed. If you
-  # need a clean shell which does not load rc file, use Shell.new instead.
+  # or create one and load the config file. If you need a clean shell which
+  # does not load config file, use Shell.new instead.
+  #
+  # @api public
   def shell
     shells.last || begin
       require_config if config_path
@@ -45,24 +49,34 @@ module Rib
 
   # All plugins which have been loaded into the memory regardless
   # it's enabled or not.
+  #
+  # @api public
   def plugins
     Shell.ancestors[1..-1].select{ |a| a.singleton_class < Plugin }
   end
 
   # Convenient way to disable all plugins in the memory.
   # This could also take a list of plugins and disable them.
+  #
+  # @api public
+  # @param plugs [Array] (Rib.plugins) Plugins which would be disabled.
   def disable_plugins plugs=plugins
     plugs.each(&:disable)
   end
 
   # Convenient way to enable all plugins in the memory.
   # This could also take a list of plugins and enable them.
+  #
+  # @api public
+  # @param plugs [Array] (Rib.plugins) Plugins which would be enabled.
   def enable_plugins plugs=plugins
     plugs.each(&:enable)
   end
 
-  # Load (actually require) ~/.config/rib/config.rb if exists.
+  # Load (actually require) the config file if it exists.
   # This might emit warnings if there's some error while loading it.
+  #
+  # @api public
   def require_config
     return unless config_path
     result = require(config_path)
@@ -73,7 +87,12 @@ module Rib
              "  #{Rib::API.format_error(e)}")
   end
 
-  # The config path where Rib tries to load upon Rib.shell
+  # The config path where Rib tries to load upon Rib.shell or
+  # Rib.require_config. It is depending on where Rib.home was discovered
+  # if no specific config path was specified via -c or --config command
+  # line option. See also Rib.config.
+  #
+  # @api public
   def config_path
     return nil unless config[:config]
     path = File.expand_path(config[:config])
@@ -85,19 +104,28 @@ module Rib
   end
 
   # Say (print to $stdout, with colors in the future, maybe)
-  # something by the name of Rib
+  # something by the name of Rib.
+  #
+  # @api public
+  # @param words [Array[String]] Words you want to say.
   def say *words
     $stdout.puts(Rib.prepare(words))
   end
 
   # Warn (print to $stderr, with colors in the future, maybe)
-  # something by the name of Rib
+  # something by the name of Rib.
+  #
+  # @api public
+  # @param words [Array[String]] Words you want to warn.
   def warn *words
     $stderr.puts(Rib.prepare(words))
   end
 
   # Warn (print to $stderr, with colors in the future, maybe)
-  # something by the name of Rib and then exit(1)
+  # something by the name of Rib and then exit(1).
+  #
+  # @api public
+  # @param words [Array[String]] Words you want to warn before aborting.
   def abort *words
     warn(words)
     exit(1)
