godfat/rib

commit b743971fad08bb259f8dabef794013ee49dbe381

avoid using TOPLEVEL_BINDING because of rubygems bin stub:
i just realized that rubygems bin stub would add two local
variables into TOPLEVEL_BINDING, one is `version', and the other
one is `str'. to avoid this, we must stop using TOPLEVEL_BINDING.

the consequence is that we can no longer rely on TOPLEVEL_BINDING,
thus we can only eval the -e argument after the shell has been
created.

the other consequence is that... nested shells can no longer
share the same local variables between shells. however since
in general we should avoid using any singleton and avoid
side effects, maybe it's really a good idea to separate bindings,
and then we would never, ever pollute TOPLEVEL_BINDING.

this could break something... please let me know if this breaks
anything. do people expect local variables could be shared
amongst shells? hopefully not...

lib/rib/runner.rb
@@ -66,7 +66,7 @@ module Rib::Runner
 
   def run argv=ARGV
     (@running_commands ||= []) << Rib.config[:name]
-    unused = parse(argv)
+    unused, e = parse(argv)
     # if it's running a Rib command, the loop would be inside Rib itself
     # so here we only parse args for the command
     return if @running_commands.pop != 'rib'
@@ -74,6 +74,7 @@ module Rib::Runner
     # not any other Rib command
     Rib.warn("Unused arguments: #{unused.inspect}") unless unused.empty?
     require 'rib/core' if Rib.config.delete(:mimic_irb)
+    Rib.shell.eval_binding.eval(e, __FILE__, __LINE__)
     loop
   end
 
@@ -96,11 +97,11 @@ module Rib::Runner
   end
 
   def parse argv
-    unused = []
+    unused, e = [], ''
     until argv.empty?
       case arg = argv.shift
       when /^-e=?(.+)?/, /^--eval=?(.+)?/
-        eval($1 || argv.shift, TOPLEVEL_BINDING, __FILE__, __LINE__)
+        e = $1 || argv.shift || ''
 
       when /^-d/, '--debug'
         $DEBUG = true
@@ -140,7 +141,7 @@ module Rib::Runner
         unused << arg
       end
     end
-    unused
+    [unused, e]
   end
 
   def parse_next argv, arg

lib/rib/version.rb
@@ -1,4 +1,4 @@
 
 module Rib
-  VERSION = '1.1.6'
+  VERSION = '1.2.0'
 end

rib.gemspec
@@ -1,14 +1,14 @@
 # -*- encoding: utf-8 -*-
-# stub: rib 1.1.6 ruby lib
+# stub: rib 1.2.0 ruby lib
 
 Gem::Specification.new do |s|
   s.name = "rib"
-  s.version = "1.1.6"
+  s.version = "1.2.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.require_paths = ["lib"]
   s.authors = ["Lin Jen-Shin (godfat)"]
-  s.date = "2014-01-06"
+  s.date = "2014-01-14"
   s.description = "Ruby-Interactive-ruBy -- Yet another interactive Ruby shell\n\nRib is based on the design of [ripl][] and the work of [ripl-rc][], some of\nthe features are also inspired by [pry][]. The aim of Rib is to be fully\nfeatured and yet very easy to opt-out or opt-in other features. It shall\nbe simple, lightweight and modular so that everyone could customize Rib.\n\n[ripl]: https://github.com/cldwalker/ripl\n[ripl-rc]: https://github.com/godfat/ripl-rc\n[pry]: https://github.com/pry/pry"
   s.email = ["godfat (XD) godfat.org"]
   s.executables = [
@@ -71,7 +71,7 @@ Gem::Specification.new do |s|
   "lib/rib/version.rb",
   "rib.gemspec",
   "screenshot.png",
-  "task/.gitignore",
+  "task/README.md",
   "task/gemgem.rb",
   "test/core/test_completion.rb",
   "test/core/test_history.rb",
@@ -83,10 +83,11 @@ Gem::Specification.new do |s|
   "test/more/test_multiline_history.rb",
   "test/test_api.rb",
   "test/test_plugin.rb",
+  "test/test_runner.rb",
   "test/test_shell.rb"]
   s.homepage = "https://github.com/godfat/rib"
   s.licenses = ["Apache License 2.0"]
-  s.rubygems_version = "2.2.0"
+  s.rubygems_version = "2.2.1"
   s.summary = "Ruby-Interactive-ruBy -- Yet another interactive Ruby shell"
   s.test_files = [
   "test/core/test_completion.rb",
@@ -99,5 +100,6 @@ Gem::Specification.new do |s|
   "test/more/test_multiline_history.rb",
   "test/test_api.rb",
   "test/test_plugin.rb",
+  "test/test_runner.rb",
   "test/test_shell.rb"]
 end

lib/rib/shell.rb
@@ -22,7 +22,7 @@ class Rib::Shell
   #   The name of the shell. Used for Rib application.
   # @option config [String] :result_prompt ('=> ')
   # @option config [String] :prompt ('>> ')
-  # @option config [Binding, Object] :binding (TOPLEVEL_BINDING)
+  # @option config [Binding, Object] :binding (new_private_binding)
   #   The context of the shell. Could be an Object.
   # @option config [Array<String>] :exit ([nil])
   #   The keywords to exit the shell. `nil` means EOF (ctrl+d).
@@ -36,12 +36,11 @@ class Rib::Shell
   # @option config [String] :autoindent_spaces ('  ')
   #   (Only if {Rib::Autoindent} plugin is used) The indented string.
   def initialize(config={})
-    self.config = {
-      :result_prompt => '=> '           ,
-      :prompt        => '>> '           ,
-      :binding       => TOPLEVEL_BINDING,
-      :exit          => [nil]           ,
-      :line          => 1               }.merge(config)
+    config[:binding] ||= new_private_binding
+    self.config = {:result_prompt => '=> ',
+                   :prompt        => '>> ',
+                   :exit          => [nil],
+                   :line          => 1    }.merge(config)
     @running = false
   end
 
@@ -65,4 +64,17 @@ class Rib::Shell
 
   protected
   attr_writer :config
+
+  private
+  # Avoid namespace pollution from rubygems bin stub.
+  # To be specific, version and str.
+  def new_private_binding
+    context = Object.new
+    def context.__rib_binding__
+      binding
+    end
+    binding = context.__rib_binding__
+    context.singleton_class.__send__(:remove_method, :__rib_binding__)
+    binding
+  end
 end

test/test_runner.rb
@@ -1,0 +1,35 @@
+
+require 'rib/test'
+require 'rib/runner'
+
+describe Rib::Runner do
+  behaves_like :rib
+
+  before do
+    Rib.disable_plugins
+    @shell = Rib::Shell.new
+    mock(Rib).shell{ @shell }.times(2)
+  end
+
+  def input *args
+    args.each{ |item| mock(@shell).get_input{ item } }
+    mock(@shell).get_input{}
+  end
+
+  def output *args
+    args.each{ |item| mock(@shell).puts("=> #{item}"){} }
+    mock(@shell).puts{}
+  end
+
+  should '-e' do
+     input('a')
+    output('1')
+    Rib::Runner.run(%w[-ea=1]).should.eq @shell
+  end
+
+  should '-e nothing' do
+     input
+    output
+    Rib::Runner.run(%w[-e]).should.eq @shell
+  end
+end

test/test_shell.rb
@@ -91,4 +91,8 @@ describe Rib::Shell do
     mock(@shell).after_loop{}
     lambda{@shell.loop}.should.raise(RuntimeError)
   end
+
+  should 'have empty binding' do
+    @shell.eval_input('local_variables').first.should.empty
+  end
 end

commit e0889ca5621c6a139bf7d68dcc21a90976fd2f4f

fix a regression in b743971fad08bb259f8dabef794013ee49dbe381
the regression was that:

    rib -e 'p 123' all

would print 123, while this won't:

    rib all -e 'p 123'

because -e was parsed by all, but without evaluating it and
all quit early. previously, since we eval immediately, this
won't happen. so we can't really delay evaluate.

the question is, i am not very sure if we could initiate the
shell when we're still paring the arguments. could this cause
issues?

we really need tests for rib apps.

lib/rib/runner.rb
@@ -66,7 +66,7 @@ module Rib::Runner
 
   def run argv=ARGV
     (@running_commands ||= []) << Rib.config[:name]
-    unused, e = parse(argv)
+    unused = parse(argv)
     # if it's running a Rib command, the loop would be inside Rib itself
     # so here we only parse args for the command
     return if @running_commands.pop != 'rib'
@@ -74,7 +74,6 @@ module Rib::Runner
     # not any other Rib command
     Rib.warn("Unused arguments: #{unused.inspect}") unless unused.empty?
     require 'rib/core' if Rib.config.delete(:mimic_irb)
-    Rib.shell.eval_binding.eval(e, __FILE__, __LINE__)
     loop
   end
 
@@ -97,11 +96,12 @@ module Rib::Runner
   end
 
   def parse argv
-    unused, e = [], ''
+    unused = []
     until argv.empty?
       case arg = argv.shift
       when /^-e=?(.+)?/, /^--eval=?(.+)?/
-        e = $1 || argv.shift || ''
+        Rib.shell.eval_binding.eval(
+          $1 || argv.shift || '', __FILE__, __LINE__)
 
       when /^-d/, '--debug'
         $DEBUG = true
@@ -141,7 +141,7 @@ module Rib::Runner
         unused << arg
       end
     end
-    [unused, e]
+    unused
   end
 
   def parse_next argv, arg

lib/rib/version.rb
@@ -1,4 +1,4 @@
 
 module Rib
-  VERSION = '1.2.3'
+  VERSION = '1.2.4'
 end

rib.gemspec
@@ -1,14 +1,14 @@
 # -*- encoding: utf-8 -*-
-# stub: rib 1.2.3 ruby lib
+# stub: rib 1.2.4 ruby lib
 
 Gem::Specification.new do |s|
   s.name = "rib"
-  s.version = "1.2.3"
+  s.version = "1.2.4"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.require_paths = ["lib"]
   s.authors = ["Lin Jen-Shin (godfat)"]
-  s.date = "2014-02-08"
+  s.date = "2014-03-12"
   s.description = "Ruby-Interactive-ruBy -- Yet another interactive Ruby shell\n\nRib is based on the design of [ripl][] and the work of [ripl-rc][], some of\nthe features are also inspired by [pry][]. The aim of Rib is to be fully\nfeatured and yet very easy to opt-out or opt-in other features. It shall\nbe simple, lightweight and modular so that everyone could customize Rib.\n\n[ripl]: https://github.com/cldwalker/ripl\n[ripl-rc]: https://github.com/godfat/ripl-rc\n[pry]: https://github.com/pry/pry"
   s.email = ["godfat (XD) godfat.org"]
   s.executables = [