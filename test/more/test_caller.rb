13b2088d33cc054b20e4e075580d6fd77590740f
Lin Jen-Shin <godfat@godfat.org>
2016-11-19 18:16:22 +0800

    Have to explicitly enable it!

--- test/more/test_caller.rb
+++ test/more/test_caller.rb
@@ -5,9 +5,11 @@ require 'rib/more/caller'
 describe Rib::Caller do
   paste :rib
 
-  would 'puts some backtrace' do
-    mock(Rib).puts(is_a(Array)){ ok }
+  test_for Rib::Caller do
+    would 'puts some backtrace' do
+      mock(Rib).puts(is_a(Array)){ ok }
 
-    Rib.caller
+      Rib.caller
+    end
   end
 end
